e8caf06feebede4619ca7a6a65c2705b726c626e
Мария <Maria@MacBook-Pro-Maria.local>
2016-09-29 21:26:53 +0300

    29.09

--- abacaba.c
+++ abacaba.c
@@ -2,33 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*
- * 1) вокруг бинарных операторов ставьте пробелы: "i + j", а не "i+j".
- * 2) после , и ; ставьет пробел: 
- * for (i = 1; i < n; i++)
- * (int n, char* string)
- */
 
 void GenerateString (int n,char* string)
 {
     int i;
     int len;
-    string[0]='a';
-    len=strlen(string);
-    for (i=1;i<n;i++)
+    string[0] = 'a';
+    len = strlen(string);
+    for (i = 1; i < n; i++)
     {
-        string[len]=string[0]+i;
-        strncpy(string+len+1,string,len);
-        len= strlen(string);
+        string[len] = string[0] + i;
+        strncpy(string + len + 1, string, len);
+        len = strlen(string);
     }
-    printf("%s",string);
+    printf("%s", string);
 }
 int main()
 {
     int n;
     char* string;
-    string=(char*) malloc((1<<n)*sizeof(char));
-    scanf("%d",&n);
-    GenerateString (n,string);
+    string = (char*) malloc((1 << n) * sizeof(char));
+    scanf("%d", &n);
+    GenerateString (n, string);
     return 0;
 }
--- split.c
+++ split.c
@@ -2,28 +2,26 @@
 #include <string.h>
 #include <stdlib.h>
 
-/*
- * 1) Все магические числа вроде 20, 80 нужно заменить на константы
- * #define MAX_STRING_SIZE 80 и т.д.
- * 2) пробелы вокруг бинарных операторов (см. комментарий к abacaba)
- */
 
-void split (char* string,char* delimeters, char*** tokens,int*tokensCount);
+#define MAX_STRING_SIZE 80
+#define MAX_NUMBER_TOK 20
+
+void split (char* string, char* delimeters, char*** tokens, int* tokensCount);
 
 int main()
 {
-    char str[80];
-    char delim[]={' ', '\t', '.', '\0'};
+    char str[MAX_STRING_SIZE];
+    char delim[] = {' ', '\t', '.', '\0'};
     int i;
     char** tok;
     int count;
     
-    tok=(char**)malloc(20*sizeof(char*));
+    tok = (char**)malloc(MAX_NUMBER_TOK * sizeof(char*));
     
     printf("Enter string:\n");
     gets(str);
     split(str, delim, &tok, &count);
-    for (i=0; i<count; i++)
+    for (i = 0; i < count; i++)
         printf("%s\n", tok[i]);
     printf("numder of words: %d", count);
     free(tok);
