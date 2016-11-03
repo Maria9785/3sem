3da0d2291413f2bd7649e0fe52b5165a9749d1d5
Мария <Maria@MacBook-Pro-Maria.local>
2016-11-03 23:10:32 +0300

    3.10

---
+++ client-server/client-server.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ee
+//
+//  Created by Мария on 28.10.16.
+//  Copyright (c) 2016 Мария. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
